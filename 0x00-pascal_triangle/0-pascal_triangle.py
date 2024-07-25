44cc4d008cbbbfde65775a37a05a730380576a32
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-07-25 14:01:43 -0700

    taks 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""A script pascal's triangle"""
+"""A scrit of pascal's triangle"""
 
 
 def pascal_triangle(n):
+    """
+    returns a list of lists of numbers
+    """
+    triangle = []
 
-    triangle_list = []
-
-    if (n <= 0):
-        return triangle_list
-
+    if n <= 0:
+        return triangle
     for i in range(n):
         temp_list = []
 
         for j in range(i+1):
-            if (j == 0) or (j == i):
+            if j == 0 or j == i:
                 temp_list.append(1)
-
             else:
-                temp_list.append(
-                    triangle_list[i-1][j-1] + triangle_list[i-1][j])
-
-        triangle_list.append(temp_list)
-
-    return triangle_list
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    return triangle
