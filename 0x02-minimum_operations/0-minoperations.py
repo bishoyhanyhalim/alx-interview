59425923bbea382cf2976689b8a3f4eb212c973a
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-11 17:28:32 -0700

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''The minimum operations for task 0
+'''
+
+
+def minOperations(n):
+    '''Computes the for func
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
