7ed2f0ffce50b71224da601b3a89e67f9dae224f
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-09-12 15:24:30 -0700

    test 2

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""2D matrix rotation for the task
-"""
+"""0-rotate_2d_matrix.py task"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix task
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    n = len(matrix)
+
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - i + first][first]
+            matrix[last - i + first][first] = matrix[last][last - i + first]
+            matrix[last][last - i + first] = matrix[i][last]
+            matrix[i][last] = top
